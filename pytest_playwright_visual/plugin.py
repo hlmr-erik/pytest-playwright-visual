fcab19fa3d495137dfdd9197b653694d2e07acdb
hlmr-erik <111565013+hlmr-erik@users.noreply.github.com>
2023-11-29 16:15:34 +0100

    Update plugin.py
    -Resize pictures in case of a dimensions mismatch, so they can be compared regardless
    -Continue after making or updating screenshots for generating a larger project
    -Allow for multiple screenshots per test
    -Add current tab name (sent from testcase) to filename
    -Create a new folder when finding a new mismatch in a testcase, for later (batch) diagnosing

--- pytest_playwright_visual/plugin.py
+++ pytest_playwright_visual/plugin.py
@@ -1,21 +1,27 @@
 import sys
 import os
 import shutil
+import uuid
 from io import BytesIO
 from pathlib import Path
 from typing import Any, Callable
 import pytest
+import re
 from PIL import Image
 from pixelmatch.contrib.PIL import pixelmatch
 
 
 @pytest.fixture
 def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Callable:
-    test_name = f"{str(Path(request.node.name))}[{str(sys.platform)}]"
+    node_name = re.sub(r'-(\d+)', '', request.node.name)
+    test_name = f"{str(Path(node_name))}[{str(sys.platform)}]"
     test_dir = str(Path(request.node.name)).split('[', 1)[0]
 
-    def compare(img: bytes, *, threshold: float = 0.1, name=f'{test_name}.png', fail_fast=False) -> None:
+    def compare(img: bytes, *, threshold: float = 0.9, current_tab_name: str, fail_fast=False, num: int = None) -> None:
         update_snapshot = pytestconfig.getoption("--update-snapshots")
+        node_name = re.sub(r'-(\d+)', '', request.node.name)
+        test_name = f"{str(Path(node_name))}[{str(sys.platform)}]"
+        test_dir = str(Path(request.node.name)).split('[', 1)[0]
         test_file_name = str(os.path.basename(Path(request.node.fspath))).strip('.py')
         filepath = (
                 Path(request.node.fspath).parent.resolve()
@@ -24,7 +30,10 @@ def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Calla
                 / test_dir
         )
         filepath.mkdir(parents=True, exist_ok=True)
-        file = filepath / name
+        if num is None:
+            file = filepath / f'{test_name}_{current_tab_name}.png'
+        else:
+            file = filepath / f'{test_name}_{current_tab_name}_{num}.png'
         # Create a dir where all snapshot test failures will go
         results_dir_name = (Path(request.node.fspath).parent.resolve()
                             / "snapshot_tests_failures")
@@ -32,27 +41,40 @@ def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Calla
                             / test_file_name / test_name)
         # Remove a single test's past run dir with actual, diff and expected images
         if test_results_dir.exists():
-            shutil.rmtree(test_results_dir)
+            new_dir_name = f"{test_results_dir}_{uuid.uuid4()}"
+            test_results_dir.rename(new_dir_name)
         if update_snapshot:
             file.write_bytes(img)
-            pytest.fail("--> Snapshots updated. Please review images")
+            print("--> Snapshots updated. Please review images")
         if not file.exists():
             file.write_bytes(img)
-            # pytest.fail(
-            pytest.fail("--> New snapshot(s) created. Please review images")
-        img_a = Image.open(BytesIO(img))
-        img_b = Image.open(file)
-        img_diff = Image.new("RGBA", img_a.size)
-        mismatch = pixelmatch(img_a, img_b, img_diff, threshold=threshold, fail_fast=fail_fast)
-        if mismatch == 0:
-            return
-        else:
-            # Create new test_results folder
-            test_results_dir.mkdir(parents=True, exist_ok=True)
-            img_diff.save(f'{test_results_dir}/Diff_{name}')
-            img_a.save(f'{test_results_dir}/Actual_{name}')
-            img_b.save(f'{test_results_dir}/Expected_{name}')
-            pytest.fail("--> Snapshots DO NOT match!")
+            print("--> New snapshot(s) created. Please review images")
+
+        if file.exists() and not update_snapshot:
+            # Load the first image to get its dimensions
+            img_a = Image.open(BytesIO(img))
+            width_a, height_a = img_a.size
+
+            # Load the second image
+            img_b = Image.open(file)
+            width_b, height_b = img_b.size
+
+            # If the dimensions of the second image do not match the first image, resize it
+            if width_a != width_b or height_a != height_b:
+                img_b = img_b.resize((width_a, height_a), Image.LANCZOS)
+
+            # Continue with the rest of the comparison logic using the resized second image
+            img_diff = Image.new("RGBA", img_a.size)
+            mismatch = pixelmatch(img_a, img_b, img_diff, threshold=threshold, fail_fast=fail_fast)
+            if mismatch == 0:
+                return
+            else:
+                # Create new test_results folder
+                test_results_dir.mkdir(parents=True, exist_ok=True)
+                img_diff.save(f'{test_results_dir}/Diff_{test_name}_{current_tab_name}.png')
+                img_a.save(f'{test_results_dir}/Actual_{test_name}_{current_tab_name}.png')
+                img_b.save(f'{test_results_dir}/Expected_{test_name}_{current_tab_name}.png')
+                pytest.fail("--> Snapshots DO NOT match!")
 
     return compare
 
